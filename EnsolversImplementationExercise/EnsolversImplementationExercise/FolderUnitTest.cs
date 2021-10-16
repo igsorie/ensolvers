23723d7365b8127123ccc85ee53b55ad13d712d5
unknown <ignacio.sosa@hotmail.es>
2021-10-16 12:11:00 -0300

    Added FolderUnitTest project and method IsValidFolderNameTest

---
+++ EnsolversImplementationExercise/EnsolversImplementationExercise/FolderUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsolversImplementationExercise
+{
+    [TestClass]
+    public class FolderUnitTest
+    {
+        [TestMethod]
+        public void IsValidFolderNameTest()
+        {
+            Folder folderName = new Folder();
+            bool isValid = folderName.IsValidItemName();
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
