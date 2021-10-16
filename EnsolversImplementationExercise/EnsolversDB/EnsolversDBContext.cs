09c011b25a0c2a50567e65ad82c8a0883bbec349
unknown <ignacio.sosa@hotmail.es>
2021-10-16 18:11:04 -0300

    Added Entity Framework code

---
+++ EnsolversImplementationExercise/EnsolversDB/EnsolversDBContext.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsolversDB
+{
+    public class EnsolversDBContext : DbContext
+    {
+        public DbSet<Folder> Folders { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public EnsolversDBContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Folder>().Property(c => c.Name).IsRequired();       
+
+            modelBuilder.Entity<Item>().Property(c => c.Name).IsRequired();
+            modelBuilder.Entity<Item>().Property(c => c.Status).IsRequired();
+            
+        }
+    }
+}
---
+++ EnsolversImplementationExercise/EnsolversDB/Repositories/RepositoryBase.cs
@@ -0,0 +1,49 @@
+﻿using EnsolversDB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsolversDB.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected EnsolversDBContext RepositoryContext;
+
+        public RepositoryBase(EnsolversDBContext repositoryContext)
+        {
+            RepositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+              RepositoryContext.Set<T>()
+                .AsNoTracking() :
+              RepositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+        bool trackChanges) =>
+            !trackChanges ?
+              RepositoryContext.Set<T>()
+                .Where(expression)
+                .AsNoTracking() :
+              RepositoryContext.Set<T>()
+                .Where(expression);
+
+        public bool Exists(Expression<Func<T, bool>> expression)
+        {
+            return RepositoryContext.Set<T>().Any(expression);
+        }
+
+        public void Create(T entity) => RepositoryContext.Set<T>().Add(entity);
+
+        public void Update(T entity) => RepositoryContext.Set<T>().Update(entity);
+
+        public void Delete(T entity) => RepositoryContext.Set<T>().Remove(entity);
+
+        public void Save() => RepositoryContext.SaveChanges();
+
+    }
+}
