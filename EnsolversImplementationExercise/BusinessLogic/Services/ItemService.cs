6e9c7fe4229e065eb54a605fc6686b58b441bac0
unknown <ignacio.sosa@hotmail.es>
2021-10-16 15:06:43 -0300

    Refactor method

---
+++ EnsolversImplementationExercise/BusinessLogic/Services/ItemService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class ItemService
+    {
+        public IItemRepository itemRepository;
+        public ItemService(IItemRepository itemRepository) 
+        {
+            this.itemRepository = itemRepository;
+        }
+
+        public void IsValidItemName(Item item)
+        {
+            if (!item.IsValidItemName())
+            {
+                throw new InvalidOperationException("Error to add item. The item name its invalid");
+            }
+        }
+    }
+}
---
+++ EnsolversImplementationExercise/EnsolversImplementationExercise/ItemServiceUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic;
+using BusinessLogic.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsolversImplementationExercise
+{
+    [TestClass]
+    public class ItemServiceUnitTest
+    {
+        private ItemService itemService;
+
+        [TestInitialize]
+        public void Init()
+        {
+            IItemRepository itemRepository = new ItemRepositoryInMemoryImpl();
+            itemService = new ItemService(itemRepository);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidItemNameTest()
+        {
+            Item item = new Item();
+            item.Name = "";
+
+            itemService.IsValidItemName(item);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidItemLongNameTest()
+        {
+            Item item = new Item();
+            item.Name = "this is very long folder name, its imposible to add this.";
+
+            itemService.IsValidItemName(item);
+        }
+    }
+}
