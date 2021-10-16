66dc208e3ae08211409e2c342eb540421dc8c823
unknown <ignacio.sosa@hotmail.es>
2021-10-16 14:30:25 -0300

    Added ItemRepository Interfaz and Memory Implementation

---
+++ EnsolversImplementationExercise/BusinessLogic/ItemRepositoryInMemoryImpl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class ItemRepositoryInMemoryImpl:IItemRepository
+    {
+        public List<Item> Items { get; set; }
+        public ItemRepositoryInMemoryImpl()
+        {
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item) { }
+
+        public void UpdateItem(Item item) { }
+
+        public Item GetItem(int id) { return new Item(); }
+
+    }
+}
