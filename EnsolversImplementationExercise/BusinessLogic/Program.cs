6990d45a2e93aac6c74ef0981a0c35b9a1b46754
unknown <ignacio.sosa@hotmail.es>
2021-10-18 09:11:21 -0300

    Fix problems with Program class

--- EnsolversImplementationExercise/BusinessLogic/Program.cs
+++ EnsolversImplementationExercise/BusinessLogic/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
+}
 }
